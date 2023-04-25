c4391d0805637a0bc73ad11c6665bc3c28bc6b3a
Sudama Patel <122884026+sudamapatel@users.noreply.github.com>
2023-04-25 15:54:48 +0530

    Add files via upload

---
+++ 27 Feb/eventListener/dblclk/dblclk.js
@@ -0,0 +1,4 @@
+let btn=document.getElementById("btn");
+btn.addEventListener("dblclick",()=>{
+    alert("DblClick Me")
+})
---
+++ 27 Feb/eventListener/onKeyPress/onKeyPress.js
@@ -0,0 +1,5 @@
+let inputBox = document.getElementById('input-box') 
+let display = document.getElementById('display')
+inputBox.addEventListener('keypress', function(e){
+    display.innerText ="you have Pressed " + e.key
+})
